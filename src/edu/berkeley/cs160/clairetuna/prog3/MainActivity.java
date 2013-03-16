9853276c3f59bff3a688cce30f5782cb454ca9df
Claire Tuna <tuna.claire@gmail.com>
2013-03-15 20:37:02 -0700

    working on ASYNC test case to check train numbers

--- src/edu/berkeley/cs160/clairetuna/prog3/MainActivity.java
+++ src/edu/berkeley/cs160/clairetuna/prog3/MainActivity.java
@@ -2,24 +2,34 @@ package edu.berkeley.cs160.clairetuna.prog3;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 	ImageMap mImageMap;
+	private Document xmlDocument;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		doshit();
+
 		setContentView(R.layout.activity_main);
 	    mImageMap = (ImageMap)findViewById(R.id.map);
 	    System.out.print("clicked something");
@@ -54,29 +64,66 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	public void doshit() throws IOException{
-		String url = "www.example.com/myfile.xml";
-		HttpClient client = new DefaultHttpClient();
-		HttpResponse response;
-		
-		response = client.execute(new HttpGet(url));
-		StatusLine sl= response.getStatusLine();
-		if(sl.getStatusCode() == HttpStatus.SC_OK){
-		ByteArrayOutputStream out = new 
-		ByteArrayOutputStream();
-		response.getEntity().
-		writeTo(out);
-		out.close();
-		System.out.println( out.toString());
-		}
+	
+	protected void onPostExecute(String results)
+    {       
+        if(results != null)
+        {   
+        	try {
+	        	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	 		   	DocumentBuilder builder = factory.newDocumentBuilder();
+	 		   	InputSource is = new InputSource(new StringReader(results));
+	 	        xmlDocument = builder.parse(is); 
+	 	        
+	 	       int traincount = Integer.parseInt(xmlDocument.getElementsByTagName("traincount").item(0).getTextContent());
+	 	       System.out.println("TRAINCOUNT IS: " + traincount);
+        	}
+        	catch (Exception ex) {
+        		//do something
+        	}
+        }
+    }
+    
 
 	
+	private class BartCheckTask extends AsyncTask<String, Void, String> {
+
+    	private final String API_KEY = "TJK4-R9EV-6R8E-UW7T";
+    	private final String url = "http://api.bart.gov/api/bsa.aspx?cmd=count&key=" + API_KEY;
+    	private Document xmlDocument;
+
+        public String doInBackground(String... city)
+        {
+        		   
+        		   HttpClient httpclient = new DefaultHttpClient();
+        	       HttpResponse response;
+        	       String responseString = null;
+        	       
+        	       
+        	       try {
+        	            response = httpclient.execute(new HttpGet(url));
+        	            StatusLine statusLine = response.getStatusLine();
+        	            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+        	                ByteArrayOutputStream out = new ByteArrayOutputStream();
+        	                response.getEntity().writeTo(out);
+        	                out.close();
+        	                responseString = out.toString();
+        	            } else{
+        	                //Closes the connection.
+        	                response.getEntity().getContent().close();
+        	                throw new IOException(statusLine.getReasonPhrase());
+        	            }
+        	        } catch (ClientProtocolException e) {
+        	            //TODO Handle problems..
+        	        } catch (IOException e) {
+        	            //TODO Handle problems..
+        	        }
+        	        return responseString;
+           }
+        }
+        
+        
 	
-	
-	
-	
-	
-}
 }
 
 	
